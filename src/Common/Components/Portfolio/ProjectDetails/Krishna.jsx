e8c4bc4e20fb66488c6d7488e2efcdfdc1ed3e89
Jithin8223 <jithinpbabu81@gmail.com>
2026-02-25 19:09:34 +0530

    new commit3

--- src/Common/Components/Portfolio/ProjectDetails/Krishna.jsx
+++ src/Common/Components/Portfolio/ProjectDetails/Krishna.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import ProjectCommonLayout from "./ProjectCommonLayout";
 import bannerImage from "../../../../assets/Residential/krishna/Sequence 01.mp4"
-import g1 from "../../../../assets/Residential/krishna/Exterior/Exterior1.png"
+import g1 from "../../../../assets/Residential/krishna/exterior/Exterior1.png"
 import g2 from "../../../../assets/Residential/krishna/exterior/Exterior2.png"
 import g3 from "../../../../assets/Residential/krishna/exterior/Exterior3.png"
-import g4 from "../../../../assets/Residential/krishna/interior/interior1.png"
-import g5 from "../../../../assets/Residential/krishna/interior/interior2.png"
-import g6 from "../../../../assets/Residential/krishna/interior/interior3.png"
-import g7 from "../../../../assets/Residential/krishna/interior/interior4.png"
-import g8 from "../../../../assets/Residential/krishna/interior/interior5.png"
-import g9 from "../../../../assets/Residential/krishna/interior/interior6.png"
-import g10 from "../../../../assets/Residential/krishna/interior/interior7.png"
-import g11 from "../../../../assets/Residential/krishna/interior/interior8.png"
-import g12 from "../../../../assets/Residential/krishna/interior/interior9.png"
-
-
-
-
-
-
+import g4 from "../../../../assets/Residential/krishna/interior/Interior1.png"
+import g5 from "../../../../assets/Residential/krishna/interior/Interior2.png"
+import g6 from "../../../../assets/Residential/krishna/interior/Interior3.png"
+import g7 from "../../../../assets/Residential/krishna/interior/Interior4.png"
+import g8 from "../../../../assets/Residential/krishna/interior/Interior5.png"
+import g9 from "../../../../assets/Residential/krishna/interior/Interior6.png"
+import g10 from "../../../../assets/Residential/krishna/interior/Interior7.png"
+import g11 from "../../../../assets/Residential/krishna/interior/Interior8.png"
+import g12 from "../../../../assets/Residential/krishna/interior/Interior9.png"
 
 
 // ✅ Define gallery with categories
